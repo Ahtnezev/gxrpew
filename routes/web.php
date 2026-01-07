ddea429989f513f3e281208e5e2bab09a88f6ae8
uneven74 <ahtnezev@gmail.com>
2026-01-06 18:38:24 -0600

    [F] web routes

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,14 @@ use App\Livewire\Checkout;
 Route::get('/', App\Livewire\ProductList::class)
     ->name('home');
 
+Route::get('/checkout/cart', Checkout::class)
+    ->name('checkout.cart');
+
 Route::get('/cart', fn() => view('cart'))
     ->name('cart.index');
 
 Route::get('/checkout/{product}', fn(\App\Models\Product $product) => view('checkout-product', compact('product')))
     ->name('checkout.show');
-
-Route::get('/checkout/cart', Checkout::class)
-    ->name('checkout.cart');
 
 Route::get('/payment/success', fn() => view('payments.success'))->name('payment.success');
 Route::get('/payment/failure', fn() => view('payments.failure'))->name('payment.failure');
